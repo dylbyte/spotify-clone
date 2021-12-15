88f39066e4c7d12c9f26c55c13ae0314cd108a0b
Dylan <heyitsdylan@gmail.com>
2021-12-15 08:41:33 -0800

    Playerbar component

---
+++ components/playerBar.tsx
@@ -0,0 +1,20 @@
+import { Box, Flex } from "@chakra-ui/layout";
+import { Text } from "@chakra-ui/react";
+
+const PlayerBar = () => {
+  return (
+    <Box width="100vw" height="100px" bg="gray.900" padding="10px">
+      <Flex align="center">
+        <Box padding="20px" width="30%" color="white">
+          <Text fontSize="large">Song Name</Text>
+          <Text fontSize="sm">Artist Name</Text>
+        </Box>
+        <Box width="40%" color="white">
+          Controls
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default PlayerBar;
